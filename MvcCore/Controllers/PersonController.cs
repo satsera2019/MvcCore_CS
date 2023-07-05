814d2c1547954ddefaab6a9a2ecedbf573758664
saba satseradze <saba.satseradze0@gmail.com>
2023-07-05 11:51:10 -0700

    change layout, add person views

---
+++ MvcCore/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCore.Models.Domain;
+
+namespace MvcCore.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddPerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPerson(Person person)
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcCore/Models/Domain/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcCore.Models.Domain
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
